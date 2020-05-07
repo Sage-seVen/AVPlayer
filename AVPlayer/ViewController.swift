401e9c12a92675aa3c5c85102f775f731387871c
Sage-seVen <sahilsagar.s57@gmail.com>
2020-05-07 16:18:48 +0530

    Initial Commit

---
+++ AVPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AVPlayer
+//
+//  Created by Sage_seVen on 07/05/20.
+//  Copyright © 2020 Sage_seVen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
